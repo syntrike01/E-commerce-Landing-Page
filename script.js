bf3ea948ecad7f5ec0c3ba79b5c7b9ca6a2cbc18
Akash Gogoi <gogoiakash09@gmail.com>
2025-09-09 10:45:49 +0530

    added hamburger menu

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const media = window.matchMedia('(calc(700 / 16 * 1rem))');
+const topNavMenu = document.querySelector('.topnav__menu');
+const main = document.querySelector('main');
+const body = document.querySelector('body');
+
+function openMobileMenu() {
+  btnOpen.setAttribute('aria-expanded', 'true');
+  topNavMenu.removeAttribute('inert');
+  topNavMenu.removeAttribute('style');
+  main.setAttribute('inert', '');
+  bodyScrollLockUpgrade.disableBodyScroll(body);
+  btnClose.focus();
+}
+
+function closeMobileMenu() {
+  btnOpen.setAttribute('aria-expanded', 'false');
+  topNavMenu.setAttribute('inert', '');
+  main.removeAttribute('inert');
+  bodyScrollLockUpgrade.enableBodyScroll(body);
+  btnOpen.focus();
+
+  setTimeout(() => {
+    topNavMenu.style.transition = 'none';
+  }, 500);
+}
+
+function setupTopNav(e) {
+  if (e.matches) {
+    // is mobile
+    console.log('is mobile');
+    topNavMenu.setAttribute('inert', '');
+    topNavMenu.style.transition = 'none';
+  } else {
+    // is tablet/desktop
+    console.log('is desktop');
+    closeMobileMenu();
+    topNavMenu.removeAttribute('inert');
+  }
+}
+
+setupTopNav(media);
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+media.addEventListener('change', function (e) {
+  setupTopNav(e);
+});
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -30,4 +81,3 @@ function showSlides(n) {
   thumbSlides[slideIndex - 1].className += " active";
 }
 
-console.log(typeof(slides))

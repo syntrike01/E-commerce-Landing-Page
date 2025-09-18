56764c1b67f1d7ccbe98c890b638a592f33274b8
Akash Gogoi <gogoiakash09@gmail.com>
2025-09-18 19:34:39 +0530

    added slideshow images for mobile and tablet devices

--- script.js
+++ script.js
@@ -49,35 +49,37 @@ media.addEventListener('change', function (e) {
   setupTopNav(e);
 });
 
+
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+// Next / previous controls
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+// thumbnail image controls
+function currentSlide(n){
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let thumbSlides = document.getElementsByClassName("thumbnail");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
 
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
+    if (n > slides.length){
+        slideIndex = 1;
+    }
 
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+    if (n < 1){
+        slideIndex = slides.length;
+    }
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+    for(i=0; i<slides.length; i++){
+        slides[i].style.display = "none";
+    }
 
-  for (i = 0; i < thumbSlides.length; i++) {
-    thumbSlides[i].className = thumbSlides[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-  thumbSlides[slideIndex - 1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
 }
-
